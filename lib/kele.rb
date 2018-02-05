ce7e813d4a86e803a8074ad982f7b52e291fcc54
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-02-05 12:27:31 -0800

    user-story-currentuser completed: added #get_me

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,5 +1,5 @@
 require 'httparty'
-
+require 'json'
 
 class Kele
    include HTTParty
@@ -9,6 +9,11 @@ class Kele
      response = self.class.post(api_url("sessions"), body: {"email": email, "password": password })
      @auth_token = response["auth_token"]
      raise 'Invalid email or password' if response.code == 404 || @auth_token.nil?
+   end
+
+   def get_me
+     response = self.class.get(api_url("users/me"), headers: {"authorization" => @auth_token })
+     @user_data = JSON.parse(response.body)   
    end
 
    private
